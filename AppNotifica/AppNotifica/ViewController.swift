949d56acd5a224ad96e4360af153475065dbddf5
Leandro Nunes <114433652+ouriquesl@users.noreply.github.com>
2023-06-01 11:30:50 -0300

    Add files via upload

---
+++ AppNotifica/AppNotifica/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppNotifica
+//
+//  Created by IFBIOTIC05 on 01/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
